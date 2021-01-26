a50d66911b06ae5246983262c9f89bec65b60ac7
Vlad <vlad.shaginyan@mail.ru>
2021-01-26 18:37:13 +0300

    Адаптив страницы "о нас"

--- frontend/pages/about-us.vue
+++ frontend/pages/about-us.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container">
-        <base-title title="about-us" class="mt-30"/>
+        <base-title title="about-us" class="mt-30" />
         <div class="text-box">
-            <div class="text">
+            <div class="text mr-60">
                 <p>
                     “DC&S Company” was founded in 2020 and based in Dubai. Our mission – to bring the freshest clothing,
                     shoes and accessories for all the dance community in the UAE!
@@ -16,6 +16,7 @@
                     student in Latin and Standard programs, so she understands the needs of students who are just at the
                     beginning of their dance journey up to those who are experienced pro-am competitors.
                 </p>
+                <img class="d-block d-lg-none mt-45 w-100" src="~/assets/images/about-us-photo.jpg" />
                 <p class="mt-40">
                     Oleg and Kateryna have been professional dancers for 17 years and continued their professional
                     career as dance instructors in Dubai training students in Latin, Ballroom and social dances such as
@@ -29,7 +30,7 @@
                     worn even for parties!
                 </p>
             </div>
-            <div class="photo">
+            <div class="photo d-none d-lg-block">
                 <img src="~/assets/images/about-us-photo.jpg" />
             </div>
         </div>
@@ -44,7 +45,7 @@
                     exchange anything, we can arrange this for you quickly and easily – just follow the section “Contact
                     Us” in the webpage.
                 </p>
-                <base-title title="Be Unique – be DC&S!" class="mt-30"/>
+                <base-title title="Be Unique – be DC&S!" class="mt-30" />
             </div>
         </div>
     </div>
@@ -61,11 +62,20 @@
     };
 </script>
 <style lang="less">
+    @import '../assets/css/variables.less';
+
     .text {
         .block-title {
             font-family: 'Inter-Medium';
             text-align: center;
             line-height: 77px;
+
+            @media @large {
+                max-width: 335px;
+
+                margin: 0 auto;
+                line-height: 50px;
+            }
 
             .front-title {
                 bottom: unset;
@@ -73,6 +83,10 @@
                 left: 0;
                 right: 0;
                 margin: auto;
+
+                @media @large {
+                    top: 24px;
+                }
             }
         }
     }
@@ -85,11 +99,45 @@
         justify-content: space-between;
         margin-top: 60px;
 
+        @media @large {
+            width: fit-content;
+            flex-direction: column;
+            justify-content: flex-start;
+            margin-top: 45px;
+        }
+
         .text {
             max-width: 530px;
             font-family: 'Inter-Light';
             font-size: 18px;
             color: @black;
+
+            @media @medium {
+                line-height: 22px;
+            }
+
+            &.mr-60 {
+                @media (min-width: 992px) {
+                    margin-right: 60px;
+                }
+            }
+        }
+
+        .photo {
+            @media @large {
+                width: fit-content;
+                margin: 0px auto 45px;
+            }
+
+            img {
+                @media (max-width: 450px) {
+                    width: 240px;
+                }
+
+                @media (min-width: 992px) {
+                    width: 100%;
+                }
+            }
         }
     }
 </style>

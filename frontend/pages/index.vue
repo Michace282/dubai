73425d0341c77e5e67c7508ea4737410db3128e9
Vlad <vlad.shaginyan@mail.ru>
2021-02-07 04:00:11 +0300

    add present modal

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -6,8 +6,10 @@
                 Free shipping within UAE
             </div>
             <div class="col-auto head-caption">
-                <img src="~/assets/images/icons/present.svg" />
-                Get your present!
+                <a href.prevent v-b-modal.present-modal
+                    ><img src="~/assets/images/icons/present.svg" />
+                    Get your present!
+                </a>
             </div>
             <div class="col-auto head-caption">
                 <img src="~/assets/images/icons/line.svg" />
@@ -73,6 +75,26 @@
                 </div>
             </div>
         </div>
+        <b-modal id="present-modal" title="BootstrapVue" hide-footer centered>
+            <template #modal-header="{ close }">
+                <h5 class="title">Get your present!</h5>
+                <a href.prevent @click="close()">
+                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M8.32868 7.501L14.8284 1.00127C15.0572 0.772428 15.0572 0.401413 14.8284 0.172605C14.5996 -0.0562035 14.2285 -0.0562328 13.9997 0.172605L7.49999 6.67234L1.00029 0.172605C0.771451 -0.0562328 0.400436 -0.0562328 0.171628 0.172605C-0.0571801 0.401442 -0.0572094 0.772457 0.171628 1.00127L6.67133 7.50097L0.171628 14.0007C-0.0572094 14.2295 -0.0572094 14.6006 0.171628 14.8294C0.286032 14.9438 0.436003 15.001 0.585973 15.001C0.735943 15.001 0.885885 14.9438 1.00032 14.8294L7.49999 8.32966L13.9997 14.8294C14.1141 14.9438 14.2641 15.001 14.414 15.001C14.564 15.001 14.714 14.9438 14.8284 14.8294C15.0572 14.6005 15.0572 14.2295 14.8284 14.0007L8.32868 7.501Z"
+                            fill="#808080"
+                        />
+                    </svg>
+                </a>
+            </template>
+            <p>
+                Get a gift for the order when you buy in the amount of 1500 AED or more. As a gift, you can choose a bag
+                for clothes or shoes.
+            </p>
+            <div class="text-right">
+                <nuxt-link to="/catalog/" class="btn btn-black">Start buying</nuxt-link>
+            </div>
+        </b-modal>
     </div>
 </template>
 
@@ -117,6 +139,51 @@
         },
     };
 </script>
+<style lang="less">
+    #present-modal {
+        background: #ffffff69;
+
+        .modal-dialog {
+            max-width: 540px;
+
+            .modal-header {
+                padding: 30px;
+                border: 0px;
+
+                a {
+                    cursor: pointer;
+                }
+
+                h5 {
+                    margin: 0 auto;
+                    font-family: 'Inter-Medium';
+                    font-size: 24px;
+                    line-height: 29px;
+                    text-transform: uppercase;
+                    color: @black;
+                }
+            }
+
+            .modal-body {
+                padding: 0px 45px 30px 45px;
+
+                p {
+                    margin: 0px;
+                    font-family: 'Inter-Light';
+                    font-size: 18px;
+                    line-height: 22px;
+                    color: @black;
+                }
+
+                .btn-black {
+                    margin-top: 30px;
+                    font-size: 14px;
+                    padding: 11px 28px;
+                }
+            }
+        }
+    }
+</style>
 <style lang="less" scoped>
     .advantages-group {
         margin-top: 90px;
@@ -164,6 +231,10 @@
             display: flex;
             align-items: center;
             font-size: 14px;
+
+            &:nth-child(2) {
+                cursor: pointer;
+            }
 
             &:last-child {
                 margin-right: 22px;

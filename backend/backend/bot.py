ed3e8bfe5873567cd9716a92a10caf06f548428c
krulikovskiy <it@krulikovskiy.com>
2021-02-15 17:04:09 +0300

    add pay backend

--- backend/backend/bot.py
+++ backend/backend/bot.py
@@ -1,6 +1,6 @@
 from telegram.bot import Bot
 
-TOKEN = '1398340303:AAGuMpIxIurHmIUeQV-Y-v82K_Hp-iV0Hjs'
+TOKEN = '1678733652:AAGScC8WJtsIXq--nhtXT4eG477xUtXdk5s'
 GROUP_ID = '-496730455'
 
 bot = Bot(token=TOKEN)

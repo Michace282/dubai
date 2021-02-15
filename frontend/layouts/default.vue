23cc018bde4740a857fbec56b578743852793639
Vlad <vlad.shaginyan@mail.ru>
2021-02-15 16:16:10 +0300

    change confirm modal

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -40,6 +40,31 @@
                 </div>
                 <nuxt />
             </div>
+            <b-modal
+                id="confirm-order-modal"
+                ref="confirm-order-modal"
+                @hidden="
+                    $router.push({ name: 'index', query: {} });
+                "
+                hide-footer
+                centered
+            >
+                <template #modal-header="{ close }">
+                    <h5 class="title">successful order processing</h5>
+                    <a href.prevent @click="close()">
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M8.32868 7.501L14.8284 1.00127C15.0572 0.772428 15.0572 0.401413 14.8284 0.172605C14.5996 -0.0562035 14.2285 -0.0562328 13.9997 0.172605L7.49999 6.67234L1.00029 0.172605C0.771451 -0.0562328 0.400436 -0.0562328 0.171628 0.172605C-0.0571801 0.401442 -0.0572094 0.772457 0.171628 1.00127L6.67133 7.50097L0.171628 14.0007C-0.0572094 14.2295 -0.0572094 14.6006 0.171628 14.8294C0.286032 14.9438 0.436003 15.001 0.585973 15.001C0.735943 15.001 0.885885 14.9438 1.00032 14.8294L7.49999 8.32966L13.9997 14.8294C14.1141 14.9438 14.2641 15.001 14.414 15.001C14.564 15.001 14.714 14.9438 14.8284 14.8294C15.0572 14.6005 15.0572 14.2295 14.8284 14.0007L8.32868 7.501Z"
+                                fill="#808080"
+                            />
+                        </svg>
+                    </a>
+                </template>
+                <p v-html="confirmModalText"></p>
+                <div class="text-right">
+                    <nuxt-link to="/" class="btn btn-black btn-home">Home</nuxt-link>
+                </div>
+            </b-modal>
         </div>
         <the-footer />
     </div>
@@ -55,6 +80,7 @@
         data() {
             return {
                 showModal: false,
+                confirmModalText: '',
             };
         },
         created() {
@@ -78,6 +104,15 @@
             v.$nuxt.$on('show-reg-modal', () => {
                 v.showModal = 'reg';
             });
+            v.$nuxt.$on('show-confirm-modal', (text) => {
+                v.confirmModalText = text;
+                v.$refs['confirm-order-modal'].show();
+            });
+        },
+        mounted() {
+            if (this.$route.query.payment) {
+                this.$refs['confirm-order-modal'].show();
+            }
         },
         watch: {
             $route() {
@@ -114,6 +149,10 @@
                 overflow: hidden;
             }
         }
+    }
+
+    .btn-home {
+        padding: 11px 58px !important;
     }
 
     .toggle-modal-enter,
